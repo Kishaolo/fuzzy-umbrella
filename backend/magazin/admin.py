342f4db15497fcc48f4f42cb70980d2c4dbcbf3d
kishaolo <vasula.malafeev@gmail.com>
2025-01-28 16:38:02 +0500

    sdelali admin

--- backend/magazin/admin.py
+++ backend/magazin/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MyModel
 
-# Register your models here.
+@admin.register(MyModel)
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at') #pola dla otobrashenia v spiske objectov
+    list_filter = ('created_at',) #filter po date sozdania
+    search_fields = ('name',) #poisk po imeni
+    ordering = ('-created_at') #sortirovka po date sozdania
