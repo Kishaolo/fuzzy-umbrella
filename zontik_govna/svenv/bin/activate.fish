646e58eee0f7c09b5b0786fbc82812bd6ae11e1e
Kishaolo <vasula.malafeev@gmail.com>
2025-08-06 14:25:37 +0300

    vse poletelo

--- zontik_govna/svenv/bin/activate.fish
+++ zontik_govna/svenv/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/); you cannot run it directly.
+# (https://fishshell.com/). You cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kisha_o1/programmirovenie/python/django_project/sproject/zontik_govna/svenv
+set -gx VIRTUAL_ENV /home/kishaolo/python/projecti/django_project/zontik_govna/svenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
