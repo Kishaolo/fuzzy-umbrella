ccec7839571ef2a3ef9d39d332b9f71ede911148
Kishaolo <vasula.malafeev@gmail.com>
2025-08-08 11:15:33 +0300

    proverka dla taga

--- zontik_govna/mysite/blog/views.py
+++ zontik_govna/mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.gereric import List_View
 
 # Create your views here.
 def post_detail(request, year, month, day, post):
@@ -32,21 +33,11 @@ def post_detail(request, year, month, day, post):
             {'post': post}
             )
 
-def post_list(request):
-    post_list = Post.published.all()
-    # разбивка  3 поста на странице
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        #информирует если переменная page не равна int
-        posts = paginator.page(1)
-    except EmptyPage:
-        #sdelat posledniu str 
-        posts = paginator.page(paginator.num_pages)
-    return render(
-            request, 
-            'blog/post/list.html', 
-            {'posts': posts}
-            )
+class PostListView(ListView):
+    """
+    Alternativa funktsiam
+    """
+    queryset = Post.published.all()
+    context_object_name = "post"
+    paginate_by = 3 
+    template_name = "blog/post/list.html"
